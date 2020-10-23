8f4608f0be83e8fa77afe778a934db9c6abea049
inventive-view-7245 <ori.markowitz@gmail.com>
2020-10-23 13:20:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  index = 0
+  squared_array = []
+  while index < array.length do
+    squared_array.push(array[index] ** 2)
+    index += 1
+  end
 end
